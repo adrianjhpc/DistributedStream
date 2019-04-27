2260a02c38c567305acc6e84f9ed6f33d3462e10
Adrian Jackson <a.jackson@epcc.ed.ac.uk>
2019-04-27 11:26:58 +0100

    Removing unncessary functionality

--- src/main_program.c
+++ src/main_program.c
@@ -279,28 +279,6 @@ void print_results(aggregate_results a_results, aggregate_results node_results, 
 
 }
 
-#if defined(__aarch64__)
-// TODO: This might be general enough to provide the functionality for any system
-// regardless of processor type given we aren't worried about thread/process migration.
-// Test on Intel systems and see if we can get rid of the architecture specificity
-// of the code.
-unsigned long get_processor_and_core(int *chip, int *core){
-	return syscall(SYS_getcpu, &core, &chip, NULL);
-}
-// TODO: Add in AMD function
-#else
-// If we're not on an ARM processor assume we're on an intel processor and use the
-// rdtscp instruction.
-unsigned long get_processor_and_core(int *chip, int *core)
-{
-	unsigned long a,d,c;
-	__asm__ volatile("rdtscp" : "=a" (a), "=d" (d), "=c" (c));
-	*chip = (c & 0xFFF000)>>12;
-	*core = c & 0xFFF;
-	return ((unsigned long)a) | (((unsigned long)d) << 32);;
-}
-#endif
-
 // The routine convert a string (name) into a number
 // for use in a MPI_Comm_split call (where the number is
 // known as a colour). It is effectively a hashing function
